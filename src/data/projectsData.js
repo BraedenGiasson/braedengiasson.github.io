2550d348c9a3b25c21953c35b0455abcac2ce39f
Giasson <2043670@johnabbottcollege.net>
2022-11-26 03:26:05 -0500

    Projects section done except photos

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,7 +11,7 @@ export const projectsData = [
     {
         id: 1,
         projectName: 'Invoicing App',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
+        projectDesc: 'An invoicing app for a customer to conveniently track and modify invoices.',
         tags: ['Java', 'Android Studio'],
         code: 'https://github.com/BraedenGiasson/AppDev2/tree/main/Invoice%20App',
         demo: 'https://github.com/BraedenGiasson/AppDev2/tree/main/Invoice%20App',
@@ -20,8 +20,8 @@ export const projectsData = [
     {
         id: 2,
         projectName: 'Budgeting Application',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['C#', 'SQLite', 'Agile', 'Scrum', 'CI/CD', 'MVP'],
+        projectDesc: 'A Desktop home budgeting application for the use of tracking and modifying budgets.',
+        tags: ['C#', 'SQLite', 'WPF', 'Agile', 'Scrum', 'CI/CD', 'MVP'],
         code: 'https://github.com/JosephAmbayec/Car-Part-Inventory-Tracker',
         demo: 'https://github.com/JosephAmbayec/Car-Part-Inventory-Tracker',
         image: two
@@ -29,7 +29,7 @@ export const projectsData = [
     {
         id: 3,
         projectName: 'Angry Birds',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
+        projectDesc: 'A simple, modified clone of the Angry Birds game, but built almost purely in JavaScript!',
         tags: ['JavaScript', 'HTML', 'CSS', 'JSON'],
         code: 'https://github.com/BraedenGiasson/Game-Programming/tree/main/Angry%20Birds',
         demo: 'https://github.com/BraedenGiasson/Game-Programming/tree/main/Angry%20Birds',
@@ -38,16 +38,16 @@ export const projectsData = [
     {
         id: 4,
         projectName: 'Car Parts Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
+        projectDesc: 'A car part tracker to track car parts. A user can create projects containing multiple car parts for their own personal projects.',
+        tags: ['JavaScript', 'HTML', 'CSS', 'MySQL', 'Bootstrap', 'Handlebars', 'Express.js'],
         code: 'https://github.com/BraedenGiasson/Car-Part-Inventory-Tracker',
         demo: 'https://github.com/BraedenGiasson/Car-Part-Inventory-Tracker',
         image: four
     },
     {
         id: 5,
-        projectName: 'Mario',
-        projectDesc: 'A Simple E-commerce application',
+        projectName: 'Super Alien Bros',
+        projectDesc: 'Technically Super Mario Bros, but with a little twist, aliens! Basically the same functionality, but cooler, with invisibility!\nCheck it out!',
         tags: ['JavaScript', 'HTML', 'CSS', 'JSON'],
         code: 'https://github.com/BraedenGiasson/Game-Programming/tree/main/Mario',
         demo: 'https://github.com/BraedenGiasson/Game-Programming/tree/main/Mario',
@@ -56,7 +56,7 @@ export const projectsData = [
     {
         id: 6,
         projectName: 'Pokemon',
-        projectDesc: 'Uber clone',
+        projectDesc: 'A small part of the original Pokemon game, focused more on the fighting between the different Pokemon.',
         tags: ['JavaScript', 'HTML', 'CSS', 'JSON'],
         code: 'https://github.com/BraedenGiasson/Game-Programming/tree/main/Pokemon',
         demo: 'https://github.com/BraedenGiasson/Game-Programming/tree/main/Pokemon',
@@ -65,7 +65,7 @@ export const projectsData = [
     {
         id: 7,
         projectName: 'Match 3',
-        projectDesc: 'A simple stock market API app',
+        projectDesc: 'Similar to Candy Crush. The goal is to match any three tiles of the same pattern by swapping two adjacent tiles. You gain points for every match of 3 to hopefully progress to the next level.',
         tags: ['JavaScript', 'HTML', 'CSS', 'JSON'],
         code: 'https://github.com/BraedenGiasson/Game-Programming/tree/main/Match%203',
         demo: 'https://github.com/BraedenGiasson/Game-Programming/tree/main/Match%203',
@@ -74,7 +74,7 @@ export const projectsData = [
     {
         id: 8,
         projectName: 'REM Destination Tracker',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+        projectDesc: 'A simple maps destination tracker where you can enter a start and end station, as well as a start date and time. It will then show you a path to the destination, including all the stops along the way, as well as the time to destination.',
         tags: ['JavaScript', 'HTML', 'CSS', 'Node.js', 'JSON'],
         code: 'https://github.com/BraedenGiasson/REM-Destination-Tracker',
         demo: 'https://github.com/BraedenGiasson/REM-Destination-Tracker',
