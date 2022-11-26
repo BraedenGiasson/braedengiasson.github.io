53dd4d0e095945f2f6562b67acdad0358be348f7
Giasson <2043670@johnabbottcollege.net>
2022-11-26 00:12:16 -0500

    Some changes in different files'

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-    email: 'janedoe.test@gmail.com',
-    phone: '+915588776600',
-    address: 'Menlo Park, California, United States - 673822 ',
+    email: 'braedengiasson@gmail.com',
+    phone: '+1 (514)-979-0095',
+    address: 'Montreal, QC, Canada',
 
     sheetAPI: ''
 }
