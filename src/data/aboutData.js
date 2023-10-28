55421b1aff9147e1bd9997ca56972cd57d6deba5
Braeden <braedengiasson@gmail.com>
2023-10-27 23:42:37 -0400

    Updated data

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
     title: "Who am I?",
-    description1: "Hey, I'm Braeden. I'm a full stack software developer from Montréal, Canada",
-    description2: "I'm currently in my last year of Computer Science at John Abbott College. I enjoy spending my time coding, traveling, learning languages, and hanging with friends. I love to challenge myself to continuously grow and increase my knowledge in the Computer Science field.",
+    description1: "Hey, I'm Braeden. I'm a full stack software developer from Montréal, Canada.",
+    description2: "I recently graduated from Computer Science Technology at John Abbott College this year (2023), and I plan to study Computer Science at Concordia University in the fall of 2024. \nI enjoy spending my time coding, traveling, learning languages, and hanging with friends/family. I love to challenge myself to continuously grow and increase my knowledge in the Computer Science field.",
     description3: 'Now that you know me, let me get to know ',
     image: 1
 }
